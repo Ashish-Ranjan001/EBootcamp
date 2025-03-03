e851c3d6f299c34612758ac0ee1ac37b00c94e2f
Ashish Ranjan <ashishranjancu17@gmail.com>
2025-03-03 12:01:27 +0530

    Basic changes in home

---
+++ Angular/demo/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+ title:any="Ashish Ranjan"
+ imagepath:any="https://picsum.photos/200/300"
+}
